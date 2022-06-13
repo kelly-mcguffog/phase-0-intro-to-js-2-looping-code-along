7a724184015b087fa90c3e347e4bfe52f83941d4
Kelly McGuffog <kellymcguffog@gmail.com>
2022-06-13 13:55:18 -0400

    commit tree edb87a212903c8077c7feb35cd73fb6c247f39da

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let messages = [];
+function writeCards(arrayOfNames, eventName){
+    for(let i=0; i < arrayOfNames.length; i++){
+        let cardMessage = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(cardMessage);
+    }
+    return messages;
+}
+
+function countDown(){
+    let countDownNumber = 10;
+    while(countDownNumber >= 0){
+        countDownNumber--;
+        console.log(countDownNumber);
+    }
+}
